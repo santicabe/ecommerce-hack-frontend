13e6e11305101667d4596aca9de7ce4b94dc4949
trottafede <ftrotta18@gmail.com>
2021-05-18 13:40:00 -0300

    single article

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import Footer from "../extra_components/Footer";
+import SingleArticle from "../extra_components/SingleArticle";
+
 function Home() {
   return (
     <div>
+      <SingleArticle />
       <Footer />
     </div>
   );
