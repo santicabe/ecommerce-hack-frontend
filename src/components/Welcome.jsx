ed0b96496314ec022044c5fa2f5472785629ae9d
JoaquinEtchegaray <joaquines98@hotmail.com>
2021-05-18 10:56:41 -0300

    added template to project

--- src/components/Welcome.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-const MensajeBienvenida = ({ nombre }) => <h1>¡Bienvenido/a a React, {nombre}!</h1>
-
-export default MensajeBienvenida
