7957c0c57ef400c4d1eb4b97651b1239b7882258
JoaquinEtchegaray <joaquines98@hotmail.com>
2021-05-18 12:02:05 -0300

    created folders

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import MensajeBienvenida from '../src/components/Welcome'
+
 
 function App() {
   return (
     <div className="App">
-      <MensajeBienvenida nombre="Martina" />,
-      <MensajeBienvenida nombre="Nicolas" />,
-      <MensajeBienvenida nombre="Juan" />,
+      
     </div>
   );
 }
---
+++ src/components/extra_components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+      <div>
+            <header className="header-v2">
+                
+        </header>
+      </div>
+    );
+}
+
+export default Navbar
