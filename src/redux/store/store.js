2eea9c3dbd9b14c39ae4adceef906d9cc74c795c
santicabe <santiago.cabe@gmail.com>
2021-05-20 12:54:37 -0300

    Cart reducer

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -3,6 +3,7 @@ import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import userReducer from "../reducers/userReducer";
 import productReducer from "../reducers/productReducer";
+import cartReducer from "../reducers/cartReducer";
 
 const persistConfig = {
   key: "root",
@@ -12,6 +13,7 @@ const persistConfig = {
 const rootReducer = combineReducers({
   userReducer,
   productReducer,
+  cartReducer,
 });
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
