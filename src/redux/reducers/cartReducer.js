7e212c8db70fe3cad0a79cafef849f77df733edd
trottafede <ftrotta18@gmail.com>
2021-05-26 15:15:14 -0300

    Todo mal el store

--- src/redux/reducers/cartReducer.js
+++ src/redux/reducers/cartReducer.js
@@ -1,11 +1,16 @@
 import produce from "immer";
+
 const INITIAL_STATE = [];
 
 const cartReducer = produce((state, action) => {
   switch (action.type) {
     case "ADD_PRODUCT": //actions.products ?
-      console.log(state);
       return [...state, action.payload];
+
+    case "ADD_QUANTITY": //actions.products ?
+      console.log(state);
+      return [...state, action.payload.quantity + 1];
+
     case "CLEAR_PRODUCTS":
       return []; //es la mejor practica???
     default:
