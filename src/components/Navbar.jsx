348c40b27f9f9324e07dbd9b54c44546aa16ec3b
JoaquinEtchegaray <joaquines98@hotmail.com>
2021-05-28 15:06:02 -0300

    css arreglos

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -78,7 +78,7 @@ function Navbar() {
                 </div>
                 <div className="flex-c-m h-full p-lr-10 ">
                   <div
-                    className="icon-header-item cl2 hov-cl1 trans-04 p-lr-11 icon-header-noti js-show-cart"
+                    className="icon-header-item cl2 hov-cl1 trans-04 p-lr-11 icon-header-noti js-show-cart mr-2"
                     data-notify={productsAmount}
                   >
                     <Link to="/cart">
@@ -86,10 +86,12 @@ function Navbar() {
                     </Link>
                   </div>
                 </div>
-                <div className="flex-c-m h-full p-l-18 p-r-25 bor5">
-                  <div className="icon-header-item cl2 hov-cl1 trans-04 p-lr-11 js-show-sidebar">
-                    <i className="zmdi zmdi-menu"></i>
-                  </div>
+
+                <div
+                  className="flex-c-m h-full p-l-18 p-r-25 bor5"
+                  style={{ fontFamily: "Poppins-Regular", fontWeight: "80px" }}
+                >
+                  Logout
                 </div>
               </div>
             </nav>
@@ -128,11 +130,48 @@ function Navbar() {
               </div>
             </div>
           </div>
-          <div className="btn-show-menu-mobile hamburger hamburger--squeeze">
+          <button
+            class="navbar-toggler second-button"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent23"
+            aria-controls="navbarSupportedContent23"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <div class="animated-icon2">
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent23">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">
+                  Home <span class="sr-only">(current)</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Features
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Pricing
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          {/* <div className="btn-show-menu-mobile hamburger hamburger--squeeze">
             <span className="hamburger-box">
               <span className="hamburger-inner"></span>
             </span>
-          </div>
+          </div> */}
           <div className="menu-mobile">
             <ul className="main-menu-m">
               <li className="active-menu">
