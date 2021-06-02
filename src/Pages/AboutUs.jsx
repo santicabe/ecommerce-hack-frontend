1e15465d2f8bf1f258f49bb062750dc10918357d
JoaquinEtchegaray <joaquines98@hotmail.com>
2021-06-02 20:08:06 -0300

    push ABOUT US

---
+++ src/Pages/AboutUs.jsx
@@ -0,0 +1,129 @@
+import React from "react";
+import "../cozastore/css/main.css";
+
+function AboutUs() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col text-center aboutUsTitle mt-5">
+          <h1
+            style={{
+              fontFamily: "Poppins-Regular",
+            }}
+          >
+            ABOUT US
+          </h1>
+        </div>
+        <div className="row mt-5 mb-5 ">
+          <div className="col-6">
+            <p
+              className="mt-5 mb-5"
+              style={{
+                fontFamily: "Poppins-Regular",
+                fontWeight: "80px",
+              }}
+            >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
+              fugit quibusdam unde maiores est corporis, similique optio
+              deleniti officia eveniet numquam nihil incidunt neque, doloremque
+              mollitia. In dolorem nam eveniet doloribus. Dolorum provident
+              quae, doloribus sunt harum amet, odit obcaecati quos sequi labore
+              neque quas iure corporis debitis. Non a placeat dolor quae quos
+              quod, iusto exercitationem quia cum, fugiat, ut vel officia. Et
+              eveniet praesentium architecto molestias sint voluptate neque, ut
+              unde molestiae illum id eum laboriosam libero aliquid,
+              reprehenderit ducimus officiis rem? Nemo error natus distinctio
+              eos velit cumque ullam nam libero, officia veniam voluptatibus
+              perferendis ut asperiores.
+            </p>
+          </div>
+          <div className="col-6 aboutUsImage"></div>
+        </div>
+        <div className="filaNosotros row text-center mb-5 ">
+          <div className="col-3 border ">
+            <img
+              src="https://media-exp1.licdn.com/dms/image/C5603AQFoXJumG85l2Q/profile-displayphoto-shrink_200_200/0/1622126042686?e=1628121600&v=beta&t=KXIEk6faYY0Dpo6X6iplZr_z_j6BReefo2JLdNTFPXU"
+              alt="imagen joaco"
+              className="rounded-circle  w-75"
+            />
+
+            <h5
+              style={{
+                fontFamily: "Poppins-Regular",
+              }}
+              className="mt-3"
+            >
+              Joaquin Etchegaray
+            </h5>
+
+            <span>
+              <i class="fa fa-linkedin-square" aria-hidden="true"></i>
+            </span>
+          </div>
+          <div className="col-3 border">
+            <img
+              src="https://media-exp1.licdn.com/dms/image/C5603AQFoXJumG85l2Q/profile-displayphoto-shrink_200_200/0/1622126042686?e=1628121600&v=beta&t=KXIEk6faYY0Dpo6X6iplZr_z_j6BReefo2JLdNTFPXU"
+              alt="imagen joaco"
+              className="rounded-circle w-75"
+            />
+
+            <h5
+              style={{
+                fontFamily: "Poppins-Regular",
+              }}
+              className="mt-3"
+            >
+              Joaquin Etchegaray
+            </h5>
+
+            <span>
+              <i class="fa fa-linkedin-square" aria-hidden="true"></i>
+            </span>
+          </div>
+          <div className="col-3 border">
+            <img
+              src="https://media-exp1.licdn.com/dms/image/C5603AQFoXJumG85l2Q/profile-displayphoto-shrink_200_200/0/1622126042686?e=1628121600&v=beta&t=KXIEk6faYY0Dpo6X6iplZr_z_j6BReefo2JLdNTFPXU"
+              alt="imagen joaco"
+              className="rounded-circle w-75"
+            />
+
+            <h5
+              style={{
+                fontFamily: "Poppins-Regular",
+              }}
+              className="mt-3"
+            >
+              Joaquin Etchegaray
+            </h5>
+
+            <span>
+              <i class="fa fa-linkedin-square" aria-hidden="true"></i>
+            </span>
+          </div>
+          <div className="col-3 border">
+            <img
+              src="https://media-exp1.licdn.com/dms/image/C5603AQFoXJumG85l2Q/profile-displayphoto-shrink_200_200/0/1622126042686?e=1628121600&v=beta&t=KXIEk6faYY0Dpo6X6iplZr_z_j6BReefo2JLdNTFPXU"
+              alt="imagen joaco"
+              className="rounded-circle w-75"
+            />
+
+            <h5
+              style={{
+                fontFamily: "Poppins-Regular",
+              }}
+              className="mt-3"
+            >
+              Joaquin Etchegaray
+            </h5>
+
+            <span>
+              <i class="fa fa-linkedin-square" aria-hidden="true"></i>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AboutUs;
