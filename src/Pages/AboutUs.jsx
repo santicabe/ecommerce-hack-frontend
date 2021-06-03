b6ff84caae55cf6f6b0fb17749c0d443612b089a
JoaquinEtchegaray <joaquines98@hotmail.com>
2021-06-03 12:20:01 -0300

    about us fotos

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "../cozastore/css/main.css";
 
+
 function AboutUs() {
   return (
     <div className="container">
@@ -39,87 +40,109 @@ function AboutUs() {
           </div>
           <div className="col-6 aboutUsImage"></div>
         </div>
-        <div className="filaNosotros row text-center mb-5 ">
-          <div className="col-3 border ">
-            <img
-              src="https://media-exp1.licdn.com/dms/image/C5603AQFoXJumG85l2Q/profile-displayphoto-shrink_200_200/0/1622126042686?e=1628121600&v=beta&t=KXIEk6faYY0Dpo6X6iplZr_z_j6BReefo2JLdNTFPXU"
-              alt="imagen joaco"
-              className="rounded-circle  w-75"
-            />
+      </div>
+      <div className="text-center mt-5 mb-5 d-flex justify-content-between">
+        <h3
+          style={{
+            fontFamily: "Poppins-Regular",
+          }}
+        >
+          Nuestro equipo:
+        </h3>
+        <div>
+          <img
+            src="https://media-exp1.licdn.com/dms/image/C5603AQFoXJumG85l2Q/profile-displayphoto-shrink_200_200/0/1622126042686?e=1628121600&v=beta&t=KXIEk6faYY0Dpo6X6iplZr_z_j6BReefo2JLdNTFPXU"
+            alt="imagen joaco"
+            className="rounded-circle  w-75"
+          />
 
-            <h5
-              style={{
-                fontFamily: "Poppins-Regular",
-              }}
-              className="mt-3"
-            >
-              Joaquin Etchegaray
-            </h5>
+          <h5
+            style={{
+              fontFamily: "Poppins-Regular",
+            }}
+            className="mt-3"
+          >
+            Joaquin Etchegaray
+          </h5>
 
-            <span>
-              <i class="fa fa-linkedin-square" aria-hidden="true"></i>
-            </span>
-          </div>
-          <div className="col-3 border">
-            <img
-              src="https://media-exp1.licdn.com/dms/image/C5603AQFoXJumG85l2Q/profile-displayphoto-shrink_200_200/0/1622126042686?e=1628121600&v=beta&t=KXIEk6faYY0Dpo6X6iplZr_z_j6BReefo2JLdNTFPXU"
-              alt="imagen joaco"
-              className="rounded-circle w-75"
-            />
+          <a
+            href="https://www.linkedin.com/in/joaquinetchegaray/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i class="fa fa-linkedin-square linkedin" aria-hidden="true"></i>
+          </a>
+        </div>
+        <div>
+          <img
+            src="https://media-exp1.licdn.com/dms/image/C4E03AQHeYyDqh20IqA/profile-displayphoto-shrink_200_200/0/1622134340122?e=1628121600&v=beta&t=gvgqq1Yg0MUbB_XRKkq2Crb8L4lIuRjPLYmykc5NORU"
+            alt="imagen santi"
+            className="rounded-circle w-75"
+          />
 
-            <h5
-              style={{
-                fontFamily: "Poppins-Regular",
-              }}
-              className="mt-3"
-            >
-              Joaquin Etchegaray
-            </h5>
+          <h5
+            style={{
+              fontFamily: "Poppins-Regular",
+            }}
+            className="mt-3"
+          >
+            Santiago Cabezudo
+          </h5>
 
-            <span>
-              <i class="fa fa-linkedin-square" aria-hidden="true"></i>
-            </span>
-          </div>
-          <div className="col-3 border">
-            <img
-              src="https://media-exp1.licdn.com/dms/image/C5603AQFoXJumG85l2Q/profile-displayphoto-shrink_200_200/0/1622126042686?e=1628121600&v=beta&t=KXIEk6faYY0Dpo6X6iplZr_z_j6BReefo2JLdNTFPXU"
-              alt="imagen joaco"
-              className="rounded-circle w-75"
-            />
+          <a
+            href="https://www.linkedin.com/in/santiago-cabezudo/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i class="fa fa-linkedin-square linkedin" aria-hidden="true"></i>
+          </a>
+        </div>
+        <div>
+          <img
+            src="https://media-exp1.licdn.com/dms/image/C4D03AQH8CDObx87HQg/profile-displayphoto-shrink_200_200/0/1608460263512?e=1628121600&v=beta&t=909gRtW63nzakdYA196x2CcqLi52r6uobWg4mQfp-9s"
+            alt="imagen fede"
+            className="rounded-circle w-75"
+          />
 
-            <h5
-              style={{
-                fontFamily: "Poppins-Regular",
-              }}
-              className="mt-3"
-            >
-              Joaquin Etchegaray
-            </h5>
+          <h5
+            style={{
+              fontFamily: "Poppins-Regular",
+            }}
+            className="mt-3"
+          >
+            Federico Trotta
+          </h5>
 
-            <span>
-              <i class="fa fa-linkedin-square" aria-hidden="true"></i>
-            </span>
-          </div>
-          <div className="col-3 border">
-            <img
-              src="https://media-exp1.licdn.com/dms/image/C5603AQFoXJumG85l2Q/profile-displayphoto-shrink_200_200/0/1622126042686?e=1628121600&v=beta&t=KXIEk6faYY0Dpo6X6iplZr_z_j6BReefo2JLdNTFPXU"
-              alt="imagen joaco"
-              className="rounded-circle w-75"
-            />
+          <a
+            href="https://www.linkedin.com/in/trottafede/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i class="fa fa-linkedin-square linkedin" aria-hidden="true"></i>
+          </a>
+        </div>
+        <div>
+          <img
+            src="https://media-exp1.licdn.com/dms/image/C5603AQG4aSm58KO8sg/profile-displayphoto-shrink_200_200/0/1622162854946?e=1628121600&v=beta&t=M9aG3NpYAtswZNOYOU6C_kdz15ifmOvu2TrTtekCB8k"
+            alt="imagen ale"
+            className="rounded-circle w-75"
+          />
+          <h5
+            style={{
+              fontFamily: "Poppins-Regular",
+            }}
+            className="mt-3"
+          >
+            Alejandro Schimdt
+          </h5>
 
-            <h5
-              style={{
-                fontFamily: "Poppins-Regular",
-              }}
-              className="mt-3"
-            >
-              Joaquin Etchegaray
-            </h5>
-
-            <span>
-              <i class="fa fa-linkedin-square" aria-hidden="true"></i>
-            </span>
-          </div>
+          <a
+            href="https://www.linkedin.com/in/alejandro-schmidt/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i class="fa fa-linkedin-square linkedin" aria-hidden="true"></i>
+          </a>
         </div>
       </div>
     </div>
